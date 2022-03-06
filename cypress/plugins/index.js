76e69c0277377ceedda569bb096fb65aa80a0635
vitafranc93@gmail.com <vitafranc93@gmail.com>
2022-03-06 13:48:47 +0200

    test 06/03/22 t:14:00

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -21,3 +21,7 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
   //require('cypress-mochawesome-reporter/plugin')(on);
 }
+
+module.exports = (on, config) => {
+  require('cypress-mochawesome-reporter/plugin')(on);
+};
